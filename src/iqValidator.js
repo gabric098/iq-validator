32bdd990cd8f0eb2624dbdd6295e8aebdcc8b7c0
Gabriele Antonini <gabriele.antonini@gmail.com>
2017-07-20 22:31:52 +0200

    Added some unit test

---
+++ src/iqValidator.js
@@ -0,0 +1,21 @@
+const validOrDefault = (value, type, defaultValue) => {
+  if (typeof value !== type) {
+    return defaultValue;
+  }
+  return value;
+};
+
+export default class IqValidator {
+  constructor(mainRule, config) {
+    this.mainRule = validOrDefault(mainRule, 'string', '');
+    this.config = validOrDefault(config, 'Array', []);
+  }
+
+  sanitise(str) {
+    const localStr = validOrDefault(str, 'string', '');
+    if (this.mainRule === '') {
+      return localStr;
+    }
+    return localStr;
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,8 +1,37 @@
 import { assert } from 'chai';
-import awesomeFunction from '../src/awesomeModule';
+import IqValidator from '../src/iqValidator';
 
-describe('Awesome test.', () => {
-  it('should test awesome function', () => {
-    assert(awesomeFunction(1, 1) === 2, 'Not awesome :(');
+let emptyParamsInstance = () => new IqValidator();
+let invalidParamsInstance = () => new IqValidator(1, {});
+
+describe('IqValidator class.', () => {
+  let iqValidator;
+
+  it('should expose a public method called sanitise', () => {
+    iqValidator = emptyParamsInstance();
+    assert(Reflect.has(iqValidator, 'sanitise'), 'Has sanitise method');
+  });
+
+  it('should use default values empty constructor', () => {
+    iqValidator = emptyParamsInstance();
+    assert(iqValidator.mainRule === '', 'mainRule is empty string');
+    assert(Array.isArray(iqValidator.config) && iqValidator.config.length === 0, 'config is an empty array');
+  });
+
+  it('should use default values if wrong parameter types are passed to constructor', () => {
+    iqValidator = invalidParamsInstance();
+    assert(iqValidator.mainRule === '', 'mainRule is empty string');
+    assert(Array.isArray(iqValidator.config) && iqValidator.config.length === 0, 'config is an empty array');
+  });
+
+  describe('sanitise method', () => {
+    it('should default invalid params to empty string', () => {
+      iqValidator = emptyParamsInstance();
+      assert(iqValidator.sanitise({invalid:true}) === '', 'doesn\'t default to empty string');
+    });
+    it('should return unchanges string if mainRule is empty or not defined', () => {
+      iqValidator = emptyParamsInstance();
+      assert(iqValidator.sanitise('testme') === 'testme', 'returns a different string');
+    });
   });
 });
